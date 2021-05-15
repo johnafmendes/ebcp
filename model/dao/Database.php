abede1d16d1b22c3bd2637074c3801525a28f8d3
John Mendes <johnafmendes@gmail.com>
2021-05-15 11:42:15 +0100

    Database updated for new version of PHP5.3.6 or higher

--- model/dao/Database.php
+++ model/dao/Database.php
@@ -19,10 +19,10 @@ class Database{
 			self::$username = "root";
 			self::$password = "";
 		} else {
-			self::$host = "54.164.120.103";
-			self::$db_name = "ebcp";
-			self::$username = "ebcp";
-			self::$password = "McTi1402";
+			self::$host = "127.0.0.1";
+			self::$db_name = "ebcpc187_ebcp";
+			self::$username = "root";
+			self::$password = "root";
 // 			echo "teste";
 		}
 	}
@@ -33,8 +33,8 @@ class Database{
 		try{
 // 			$pdo = "mysql:host=" . self::$host . ";dbname=" . self::$db_name ."," . self::$username . "," . self::$password;
 // 			echo "[" . $pdo . "]";
-// 			$conn = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$db_name . ";charset=utf8", self::$username, self::$password);//funciona para versoes php 5.3.6 ou superior
-			$conn = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$db_name, self::$username, self::$password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			$conn = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$db_name . ";charset=utf8", self::$username, self::$password);//funciona para versoes php 5.3.6 ou superior
+			// $conn = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$db_name, self::$username, self::$password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(PDOException $e){
 			echo "Connection error: " . $e->getMessage();
